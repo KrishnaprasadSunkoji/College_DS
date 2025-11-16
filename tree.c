f5b2775d5eca090f0f3833d75da357e7bae0655c
KrishnaprasadSunkoji <github.krishnaprasad@gmail.com>
2025-11-16 22:10:25 +0530

    Added Tree

---
+++ tree.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int value)
+{
+    struct Node* n = malloc(sizeof(struct Node));
+    n->data = value;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void preorder(struct Node* root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void inorder(struct Node* root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void postorder(struct Node* root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main()
+{
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+
+    printf("Preorder: "); preorder(root);
+    printf("\nInorder: "); inorder(root);
+    printf("\nPostorder: "); postorder(root);
+
+    return 0;
+}
